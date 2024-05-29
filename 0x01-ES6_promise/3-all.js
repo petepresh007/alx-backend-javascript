a4dc7c31115a6fc1244fd635e37924a9e85b3515
Precious Peter <peterprecious370@gmail.com>
2024-05-29 10:49:08 -0700

    resolve with .all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((response) => console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`))
+    .catch(() => new Error('Signup system offline'));
+}
