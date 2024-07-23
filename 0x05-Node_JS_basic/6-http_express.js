3520c840b8d51a499acb78da17979bdda65f2bdf
petepresh007 <preshtech18@gmail.com>
2024-07-23 19:08:27 +0100

    express http

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express('');
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('app listening on port: 1245');
+});
