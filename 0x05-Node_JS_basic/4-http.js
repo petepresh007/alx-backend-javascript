13f17c91b81fcf3811bede56823b9c583a31bb16
petepresh007 <preshtech18@gmail.com>
2024-07-23 17:37:51 +0100

    hopefull fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,16 @@
 const http = require('node:http');
 
-const port = '1245';
+const HOST = 'localhost';
+const PORT = '1245';
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
-app.listen(port);
+
+app.listen(PORT, HOST, () => {
+  console.log(`app listening on port ${PORT}`);
+});
 
 module.exports = app;
