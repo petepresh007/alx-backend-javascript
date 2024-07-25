f4f6ba5f32f9fafe7d16d2bd7e7ff3a8e3953b3a
petepresh007 <preshtech18@gmail.com>
2024-07-25 18:54:38 +0100

    final fix

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,11 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('returns a promise with the correct response if success is true', () => {
-    getPaymentTokenFromAPI(true).then((res) => {
-      expect(res).to.deep.equal({ data: 'Successful response from the API' });
-      done();
-    });
-  });
+  it('getPaymentTokenFromAPI(success), where success == true', () => new Promise((done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      });
+  }));
 });
