5aba305e2ae0081e2bcdcc4b102a12496a2e2ff9
petepresh007 <preshtech18@gmail.com>
2024-07-25 13:44:02 +0100

    trying to fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,20 @@
-const calculateNumber = require('./0-calcul');
 const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('rounds provided number and returns their sum', function () {
+describe('calculateNumber', () => {
+  it('rounds provided number and returns their sum', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('should a whole number and a number with decimal point', function () {
+  it('should a whole number and a number with decimal point', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it('handle numbers with decimal points', function () {
+  it('handle numbers with decimal points', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('handle more decimals', function () {
+  it('handle more decimals', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  it('handle zeo values', function () {
+  it('handle zeo values', () => {
     assert.strictEqual(calculateNumber(0, 0), 0);
     assert.strictEqual(calculateNumber(0.5, 0.5), 2);
   });
